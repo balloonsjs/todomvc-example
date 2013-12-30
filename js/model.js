4a30149512f5f055af73d0cc2dec33d3250108d2
Guille Paz <guille87paz@gmail.com>
2013-12-30 18:02:54 -0300

    Update to v0.2.0.

--- js/model.js
+++ js/model.js
@@ -10,125 +10,129 @@
             'completed': 'checked'
         };
 
-    function Todo() {
-        this._id = 0;
-        this._collection = [];
-    }
 
-    Q.observable(Todo);
+    var Todo = Q.define({
+        'name': 'todo',
 
-    Todo.prototype.add = function (text) {
-         var task = {
-            'id': this._id += 1,
-            'date': new Date(),
-            'status': 'active', // active or completed
-            'text': text,
-            'checked': ''
-         };
+        'init': function () {
+            this._id = 0;
+            this._collection = [];
+        },
 
-         this._collection.push(task);
+        'add': function (text) {
+             var task = {
+                'id': this._id += 1,
+                'date': new Date(),
+                'status': 'active', // active or completed
+                'text': text,
+                'checked': ''
+             };
 
-         this.emit('add');
+             this._collection.push(task);
 
-         return this;
-    };
+             this.emit('add');
 
-    Todo.prototype.edit = function (id, text) {
-        var tasks = this._collection,
-            i = 0,
-            len = tasks.length;
+             return this;
+        },
 
-        for (i; i < len; i += 1) {
-            if (tasks[i].id === id) {
-                tasks[i].date = new Date();
-                tasks[i].text = text;
-                break;
+        'edit': function (id, text) {
+            var tasks = this._collection,
+                i = 0,
+                len = tasks.length;
+
+            for (i; i < len; i += 1) {
+                if (tasks[i].id === id) {
+                    tasks[i].date = new Date();
+                    tasks[i].text = text;
+                    break;
+                }
             }
-        }
 
-        this.emit('edit');
+            this.emit('edit');
 
-        return this;
-    };
+            return this;
+        },
 
-    Todo.prototype.toggle = function (id) {
-        var tasks = this._collection,
-            i = 0,
-            len = tasks.length;
+        'toggle': function (id) {
+            var tasks = this._collection,
+                i = 0,
+                len = tasks.length;
 
-        for (i; i < len; i += 1) {
-            if (tasks[i].id === id) {
-                tasks[i].status = toggle[tasks[i].status];
-                tasks[i].checked = checked[tasks[i].status];
-                break;
+            for (i; i < len; i += 1) {
+                if (tasks[i].id === id) {
+                    tasks[i].status = toggle[tasks[i].status];
+                    tasks[i].checked = checked[tasks[i].status];
+                    break;
+                }
             }
-        }
 
-        this.emit('toggle');
+            this.emit('toggle');
 
-        return this;
-    };
+            return this;
+        },
 
-    Todo.prototype.toggleAll = function (completed) {
-        var tasks = this._collection,
-            i = 0,
-            len = tasks.length,
-            status = (completed) ? 'completed': 'active';
+        'toggleAll': function (completed) {
+            var tasks = this._collection,
+                i = 0,
+                len = tasks.length,
+                status = (completed) ? 'completed': 'active';
 
-        for (i; i < len; i += 1) {
-            tasks[i].status = status;
-            tasks[i].checked = checked[status];
-        }
-
-        this.emit('toggle');
-
-        return this;
-    };
-
-    Todo.prototype.remove = function (key, value) {
-
-        var tasks = this._collection,
-            i = 0,
-            len = tasks.length;
-
-        if (typeof key === 'number') {
-            value = key;
-            key = 'id';
-        }
-
-        for (i; i < len; i += 1) {
-            if (tasks[i][key] === value) {
-                tasks.splice(i, 1);
-                len -= 1;
-                i -= 1;
+            for (i; i < len; i += 1) {
+                tasks[i].status = status;
+                tasks[i].checked = checked[status];
             }
-        }
 
-        this.emit('remove');
-    };
+            this.emit('toggle');
 
-    Todo.prototype.items = function () {
-        return this._collection;
-    };
+            return this;
+        },
 
-    Todo.prototype.count = function (status) {
-        var tasks = this._collection,
-            i = 0,
-            len = tasks.length,
-            count = 0;
+        'remove': function (key, value) {
 
-        for (i; i < len; i += 1) {
-            if (tasks[i].status === status) {
-                count += 1;
+            var tasks = this._collection,
+                i = 0,
+                len = tasks.length;
+
+            if (typeof key === 'number') {
+                value = key;
+                key = 'id';
             }
+
+            for (i; i < len; i += 1) {
+                if (tasks[i][key] === value) {
+                    tasks.splice(i, 1);
+                    len -= 1;
+                    i -= 1;
+                }
+            }
+
+            this.emit('remove');
+        },
+
+        'items': function () {
+            return this._collection;
+        },
+
+        'count': function (status) {
+            var tasks = this._collection,
+                i = 0,
+                len = tasks.length,
+                count = 0;
+
+            for (i; i < len; i += 1) {
+                if (tasks[i].status === status) {
+                    count += 1;
+                }
+            }
+
+            return count;
+        },
+
+        'size': function () {
+            return this._collection.length;
         }
+    });
 
-        return count;
-    };
-
-    Todo.prototype.size = function () {
-        return this._collection.length;
-    };
 
     // Expose Todo
     window.Todo = Todo;
